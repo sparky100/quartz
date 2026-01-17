09ee3d5564739670f22cffea38abefc0393d990f
sparky100 <parkinsonsj@gmail.com>
2026-01-17 17:37:28 +0000

    Quartz sync: Jan 17, 2026, 5:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "In Our Time",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
